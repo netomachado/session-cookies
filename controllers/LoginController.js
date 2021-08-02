aa76561311234ea32d2fc1de9e9e43f626297e15
andreliman <andredelimanunes@gmail.com>
2021-08-02 18:28:27 -0300

    Login criado

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -5,6 +5,21 @@ exports.criarUsuario = (nome, email, senha) => {
   const senhaCriptografada = bcrypt.hashSync(senha);
 
   const usuario = UsuarioModel.criarUmUsuario(nome, email, senhaCriptografada);
-  
+
   return usuario;
-}
+};
+
+exports.logarUsuario = ({ email, senha }) => {
+  const usuario = UsuarioModel.listarUsuarioPorEmail({ email });
+
+  const senhaCheck = bcrypt.compareSync(senha, usuario.senha);
+
+  if (!usuario) {
+    throw new Error("Access denied");
+  }
+  if (!senhaCheck) {
+    throw new Error("Access denied");
+  }
+
+  return usuario;
+};
--- middlewares/verificarUsuarioLogado.js
+++ middlewares/verificarUsuarioLogado.js
@@ -1,7 +1,7 @@
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
   if (req.session.usuario) {
     next();
   } else {
-    res.redirect("/")
+    res.redirect("/");
   }
-}
+};
